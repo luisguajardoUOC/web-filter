9b60a0493dfb69891b450290cf5de70546834d3e
Luis Guajardo <luis.guajardo.homs@gmail.com>
2024-09-30 10:16:20 +0200

    updatre

--- src/app/pages/filters/filters.component.ts
+++ src/app/pages/filters/filters.component.ts
@@ -11,7 +11,15 @@ export class FiltersComponent  implements OnInit {
 
   filteringRules: any[] = [];
   displayedColumns: string[] = ['url', 'type', 'action'];  // Definimos las columnas a mostrar en la tabla
-
+  newRule: {
+    action: string;
+    url: string;
+    type: string;
+  } = {
+    action: 'bloquear',  // Valor por defecto
+    url: '',             // Inicialmente vacío
+    type: 'a'            // Valor por defecto
+  };
 
   constructor(private webFilterService: WebFilterService) {}
   ngOnInit(): void {
@@ -31,6 +39,18 @@ export class FiltersComponent  implements OnInit {
       this.filteringRules = data.filteringRules;
     });
   }
+  // Método para agregar una nueva regla
+  addRule(): void {
+    // Validar que la URL no esté vacía
+    if (!this.newRule.url) {
+      return;
+    }
+      // Llamamos al servicio para agregar la regla
+    this.webFilterService.addRule(this.newRule).subscribe(data => {
+      this.filteringRules = data;  // Actualizamos la lista de reglas
+      this.clearForm();  // Limpiar el formulario después de agregar
+    });
+  }
 
   // Método para eliminar una regla de filtrado
   deleteRule(id: number): void {
@@ -38,5 +58,14 @@ export class FiltersComponent  implements OnInit {
       // Al eliminar la regla, actualizamos la lista
       this.filteringRules = this.filteringRules.filter(rule => rule.id !== id);
     });
+  }
+
+  // Limpiar el formulario
+  clearForm(): void {
+    this.newRule = {
+      action: 'bloquear',  // Valor por defecto
+      url: '',             // Inicialmente vacío
+      type: 'a'            // Valor por defecto
+    };
   }
 }
